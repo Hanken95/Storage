a873e1de42414587eebef16e640b90bd4bb50e2e
Luna <hanken95@gmail.com>
2024-10-24 20:23:31 +0200

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Range(1, 1000000)]
+        public int Price { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime Orderdate { get; set; } 
+
+        [Required]
+        [StringLength(50)]
+        public string Category { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Shelf { get; set; }
+
+        [Required]
+        public int Count { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+    }
+}
